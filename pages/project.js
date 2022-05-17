c5c7720703b2969dbc6cc054426c7a1c633427b7
skad12 <stephenkunle26@gmail.com>
2022-05-17 02:13:38 +0100

    project

--- pages/project.js
+++ pages/project.js
@@ -15,10 +15,11 @@ export default function Project() {
       <div
         className="antialiased lg:pt-5"
         style={{
-          backgroundImage: ` url('https://images.pexels.com/photos/7794425/pexels-photo-7794425.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')`,
+          backgroundImage: ` url(' https://images.pexels.com/photos/7078034/pexels-photo-7078034.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1 ')`,
         }}
       >
-        <div className="grid pt-32 pb-24 pl-16 lg:pt-48 lg:grid-cols-2 gap-y-36">
+        <div className="grid pt-32 pb-24 pl-16 lg:pt-48 lg:grid-cols-2 lg:gap-y-36 gap-y-16">
+
           {/*  content1  */}
           <div className="w-5/6 rounded-2xl gap-y-2">
             <div>
@@ -30,7 +31,7 @@ export default function Project() {
                 className="w-full h-52 rounded-2xl"
               />
             </div>
-            <div className="border-2 border-blue-500 rounded-full "></div>
+            
             <div>
               <Image
                 src={`/wapp.png`}
@@ -42,59 +43,21 @@ export default function Project() {
             </div>
           </div>
 
-          <div className="space-y-6 tracking-wide text-black lg:pt-5">
-            <div className="font-bold text-center lg:text-3xl">
+          <div className="grid content-center space-y-5 tracking-wide text-black lg:space-y-10 lg:pt-5">
+            <div className="font-bold lg:text-3xl">
               What is Lorem Ipsum?
             </div>
             <div className="font-semibold lg:text-xl">
               this is a paragraphLorem Ipsum is simply dummy text of the
               printing and typesetting industry. 
             </div>
-            <div>
-              <button className="p-3 text-white bg-blue-500 rounded-2xl hover:bg-blue-700 hover:animate-bounce">
-                Test-App
-              </button>
+            <div className="">
+            <button type="button" class="text-white bg-red-700 hover:bg-red-800 font-medium rounded-lg
+             text-sm px-5 py-2.5  hover:animate-bounce text-center mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Test</button>
             </div>
           </div>
 
-          {/* content2 */}
-
-          <div className="space-y-6 tracking-wide text-black lg:pt-5">
-            <div className="text-3xl font-bold text-center">
-              What is Lorem Ipsum?
-            </div>
-            <div className="text-xl font-semibold ">
-              this is a paragraphLorem Ipsum is simply dummy text of the
-              printing and typesetting industry. 
-            </div>
-            <div>
-              <button className="p-3 text-white bg-blue-500 rounded-2xl hover:bg-blue-700 hover:animate-bounce">
-                Test-App
-              </button>
-            </div>
-          </div>
-
-          <div className="w-5/6 rounded-2xl gap-y-2">
-            <Image
-              src={`/wapp.png`}
-              width={600}
-              height={300}
-              alt=""
-              className="w-full h-52 rounded-2xl"
-            />
-            <div className="border-2 border-blue-500 rounded-full "></div>
-            <div>
-              <Image
-                src={`/wapp.png`}
-                width={600}
-                height={300}
-                alt=""
-                className="w-full h-52 rounded-2xl"
-              />
-            </div>
-          </div>
-
-          {/* content1  */}
+         {/*  content2  */}
           <div className="w-5/6 rounded-2xl gap-y-2">
             <div>
               <Image
@@ -105,7 +68,7 @@ export default function Project() {
                 className="w-full h-52 rounded-2xl"
               />
             </div>
-            <div className="border-2 border-blue-500 rounded-full "></div>
+            
             <div>
               <Image
                 src={`/wapp.png`}
@@ -117,38 +80,21 @@ export default function Project() {
             </div>
           </div>
 
-          <div className="space-y-6 tracking-wide text-black lg:pt-5">
-            <div className="text-3xl font-bold text-center">
-              What is Lorem Ipsum?
-            </div>
-            <div className="text-xl font-semibold ">
-              this is a paragraphLorem Ipsum is simply dummy text of the
-              printing and typesetting industry. 
-            </div>
-            <div>
-              <button className="p-3 text-white bg-blue-500 rounded-2xl hover:bg-blue-700 hover:animate-bounce">
-                Test-App
-              </button>
-            </div>
-          </div>
-
-          {/* content2  */}
-
-          <div className="space-y-6 tracking-wide text-black lg:pt-5">
-            <div className="font-bold text-center lg:text-3xl">
+          <div className="grid content-center space-y-5 tracking-wide text-black lg:space-y-10 lg:pt-5">
+            <div className="font-bold lg:text-3xl">
               What is Lorem Ipsum?
             </div>
             <div className="font-semibold lg:text-xl">
               this is a paragraphLorem Ipsum is simply dummy text of the
-              printing and typesetting industry.
+              printing and typesetting industry. 
             </div>
-            <div  className="p-3 text-center text-white bg-blue-500 rounded-2xl hover:bg-blue-700 hover:animate-bounce">
-              <button>
-                Test-App
-              </button>
+            <div className="">
+            <button type="button" class="text-white bg-red-700 hover:bg-red-800 font-medium rounded-lg
+             text-sm px-5 py-2.5  hover:animate-bounce text-center mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Test</button>
             </div>
           </div>
 
+          {/*  content3  */}
           <div className="w-5/6 rounded-2xl gap-y-2">
             <div>
               <Image
@@ -159,7 +105,7 @@ export default function Project() {
                 className="w-full h-52 rounded-2xl"
               />
             </div>
-            <div className="border-2 border-blue-500 rounded-full "></div>
+            
             <div>
               <Image
                 src={`/wapp.png`}
@@ -170,6 +116,58 @@ export default function Project() {
               />
             </div>
           </div>
+
+          <div className="grid content-center space-y-5 tracking-wide text-black lg:space-y-10 lg:pt-5">
+            <div className="font-bold lg:text-3xl">
+              What is Lorem Ipsum?
+            </div>
+            <div className="font-semibold lg:text-xl">
+              this is a paragraphLorem Ipsum is simply dummy text of the
+              printing and typesetting industry. 
+            </div>
+            <div className="">
+            <button type="button" class="text-white bg-red-700 hover:bg-red-800 font-medium rounded-lg
+             text-sm px-5 py-2.5  hover:animate-bounce text-center mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Test</button>
+            </div>
+          </div>
+
+        {/*  content4  */}
+          <div className="w-5/6 rounded-2xl gap-y-2">
+            <div>
+              <Image
+                src={`/wapp.png`}
+                width={600}
+                height={300}
+                alt=""
+                className="w-full h-52 rounded-2xl"
+              />
+            </div>
+            
+            <div>
+              <Image
+                src={`/wapp.png`}
+                width={600}
+                height={300}
+                alt=""
+                className="w-full h-52 rounded-2xl"
+              />
+            </div>
+          </div>
+
+          <div className="grid content-center space-y-5 tracking-wide text-black lg:space-y-10 lg:pt-5">
+            <div className="font-bold lg:text-3xl">
+              What is Lorem Ipsum?
+            </div>
+            <div className="font-semibold lg:text-xl">
+              this is a paragraphLorem Ipsum is simply dummy text of the
+              printing and typesetting industry. 
+            </div>
+            <div className="">
+            <button type="button" class="text-white bg-red-700 hover:bg-red-800 font-medium rounded-lg
+             text-sm px-5 py-2.5  hover:animate-bounce text-center mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Test</button>
+            </div>
+          </div>
+
         </div>
       </div>
     </>
