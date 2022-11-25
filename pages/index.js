bbbe6f9cc1e6dc9b70a30ae6299d279dbfb366b5
skad12 <dammykunle79@gmail.com>
2022-11-25 06:51:21 +0100

    buybis

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ export default function Home() {
 
           <div className=" "> 
           
-      <ul className="menu  w-56 p-2 rounded-box bg-glass ">
+      <ul className="menu  w-56 p-2 rounded-box bg-glass active:bg-none focus:outline-none ">
 
   <li>
     <Link  href="/">
@@ -77,12 +77,11 @@ export default function Home() {
         </div>
             
 
-            <div className=" lg:flex justify-center lg:space-x-5 lg:pt-6">
-              {/* <div className="font-bold flex justify-center">Stack:</div> */}
-
-              <div className="flex justify-center space-x-5 lg:space-x-10">
+            <div className=" lg:flex lg:space-x-5 lg:pl-20">
+            
+              <div className="flex justify-center space-x-5 ">
             <div >
-            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" className="h-10 w-10 lg:h-32 lg:w-32 hover:h-52 hover:w-52 "
+            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" className="h-10 w-10 "
 viewBox="0 0 48 48"
 style={{color:"fill:#000000;"}}><path fill="#E65100" d="M41,5H7l3,34l14,4l14-4L41,5L41,5z"></path><path fill="#FF6D00" d="M24 8L24 39.9 35.2 36.7 37.7 8z">
   </path><path fill="#FFF" d="M24,25v-4h8.6l-0.7,11.5L24,35.1v-4.2l4.1-1.4l0.3-4.5H24z M32.9,17l0.3-4H24v4H32.9z"></path>
@@ -92,7 +91,7 @@ style={{color:"fill:#000000;"}}><path fill="#E65100" d="M41,5H7l3,34l14,4l14-4L4
   </div>
 
   <div >
-  <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" className="h-10 w-10 lg:h-32 lg:w-32 hover:h-52 hover:w-52"
+  <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" className="h-10 w-10 "
 viewBox="0 0 48 48"
 style={{color:"fill:#000000;"}}><path fill="#0277BD" d="M41,5H7l3,34l14,4l14-4L41,5L41,5z"></path><path fill="#039BE5" d="M24 8L24 39.9 35.2 36.7 37.7 8z"></path>
 <path fill="#FFF" d="M33.1 13L24 13 24 17 28.9 17 28.6 21 24 21 24 25 28.4 25 28.1 29.5 24 30.9 24 35.1 31.9 32.5 32.6 21 32.6 21z"></path>
@@ -102,7 +101,7 @@ style={{color:"fill:#000000;"}}><path fill="#0277BD" d="M41,5H7l3,34l14,4l14-4L4
   </div>
 
   <div >
-  <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" className="h-10 w-10 lg:h-32 lg:w-32 hover:h-52 hover:w-52"
+  <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" className="h-10 w-10 "
 viewBox="0 0 48 48"
 style={{color:"fill:#000000;"}}><path fill="#f7df1e" d="M6,42V6h36v36H6z"></path>
 <path fill="#000001" d="M29.538,32.947c0.692,1.124,1.444,2.201,3.037,2.201c1.338,0,2.04-0.665,2.04-1.585 c0-1.101-0.726-1.492-2.198-2.133l-0.807-0.344c-2.329-0.988-3.878-2.226-3.878-4.841c0-2.41,1.845-4.244,4.728-4.244 c2.053,0,3.528,0.711,4.592,2.573l-2.514,1.607c-0.553-0.988-1.151-1.377-2.078-1.377c-0.946,0-1.545,0.597-1.545,1.377 c0,0.964,0.6,1.354,1.985,1.951l0.807,0.344C36.452,29.645,38,30.839,38,33.523C38,36.415,35.716,38,32.65,38 c-2.999,0-4.702-1.505-5.65-3.368L29.538,32.947z M17.952,33.029c0.506,0.906,1.275,1.603,2.381,1.603 c1.058,0,1.667-0.418,1.667-2.043V22h3.333v11.101c0,3.367-1.953,4.899-4.805,4.899c-2.577,0-4.437-1.746-5.195-3.368 L17.952,33.029z">
@@ -110,7 +109,7 @@ style={{color:"fill:#000000;"}}><path fill="#f7df1e" d="M6,42V6h36v36H6z"></path
   </div>
 
   <div >
-  <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" className="h-10 w-10 lg:h-32 lg:w-32 hover:h-52 hover:w-52"
+  <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" className="h-10 w-10 "
 viewBox="0 0 48 48"
 style={{color:"fill:#000000;"}}><path fill="#80deea" d="M24,34C11.1,34,1,29.6,1,24c0-5.6,10.1-10,23-10c12.9,0,23,4.4,23,10C47,29.6,36.9,34,24,34z M24,16	c-12.6,0-21,4.1-21,8c0,3.9,8.4,8,21,8s21-4.1,21-8C45,20.1,36.6,16,24,16z"></path>
 <path fill="#80deea" d="M15.1,44.6c-1,0-1.8-0.2-2.6-0.7C7.6,41.1,8.9,30.2,15.3,19l0,0c3-5.2,6.7-9.6,10.3-12.4c3.9-3,7.4-3.9,9.8-2.5	c2.5,1.4,3.4,4.9,2.8,9.8c-0.6,4.6-2.6,10-5.6,15.2c-3,5.2-6.7,9.6-10.3,12.4C19.7,43.5,17.2,44.6,15.1,44.6z M32.9,5.4	c-1.6,0-3.7,0.9-6,2.7c-3.4,2.7-6.9,6.9-9.8,11.9l0,0c-6.3,10.9-6.9,20.3-3.6,22.2c1.7,1,4.5,0.1,7.6-2.3c3.4-2.7,6.9-6.9,9.8-11.9	c2.9-5,4.8-10.1,5.4-14.4c0.5-4-0.1-6.8-1.8-7.8C34,5.6,33.5,5.4,32.9,5.4z"></path>
@@ -120,16 +119,23 @@ style={{color:"fill:#000000;"}}><path fill="#80deea" d="M24,34C11.1,34,1,29.6,1,
   </div>
 
   <div >
-  <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" className="h-10 w-10 lg:h-32 lg:w-32 hover:h-52 hover:w-52"
+  <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" className="h-10 w-10"
 viewBox="0 0 48 48"
 style={{color:"fill:#000000;"}}><path fill="#212121" d="M18.974,31.5c0,0.828-0.671,1.5-1.5,1.5s-1.5-0.672-1.5-1.5v-14c0-0.653,0.423-1.231,1.045-1.43 c0.625-0.198,1.302,0.03,1.679,0.563l16.777,23.704C40.617,36.709,44,30.735,44,24c0-11-9-20-20-20S4,13,4,24s9,20,20,20 c3.192,0,6.206-0.777,8.89-2.122L18.974,22.216V31.5z M28.974,16.5c0-0.828,0.671-1.5,1.5-1.5s1.5,0.672,1.5,1.5v13.84l-3-4.227 V16.5z">
   </path></svg>
   </div>
-</div>
 
-            </div>
-
-
+        <div>
+          <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+       className="h-10 w-10"
+      viewBox="0 0 48 48">
+      <path fill="#00acc1" d="M24,9.604c-6.4,0-10.4,3.199-12,9.597c2.4-3.199,5.2-4.398,8.4-3.599 c1.826,0.456,3.131,1.781,4.576,3.247C27.328,21.236,30.051,24,36,24c6.4,0,10.4-3.199,12-9.598c-2.4,3.199-5.2,4.399-8.4,3.6 c-1.825-0.456-3.13-1.781-4.575-3.247C32.672,12.367,29.948,9.604,24,9.604L24,9.604z M12,24c-6.4,0-10.4,3.199-12,9.598 c2.4-3.199,5.2-4.399,8.4-3.599c1.825,0.457,3.13,1.781,4.575,3.246c2.353,2.388,5.077,5.152,11.025,5.152 c6.4,0,10.4-3.199,12-9.598c-2.4,3.199-5.2,4.399-8.4,3.599c-1.826-0.456-3.131-1.781-4.576-3.246C20.672,26.764,17.949,24,12,24 L12,24z">
+      </path>
+      </svg>
+        </div>
+        
+         </div>
+        </div>
       </div>
     </>
   );
